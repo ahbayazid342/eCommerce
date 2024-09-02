5d0b315b4e0a6c7859e48f5a80bb94c7b8346ba0
bayazid <bayazid133340@gmail.com>
2024-09-02 18:46:56 +0600

    loading reducer added

--- redux/store.ts
+++ redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./features/cartSlice";
 import productReducer from "./features/productSlice";
+import loadingReducer from "./features/loadingSlice";
 
 export const store = configureStore({
   reducer: {
     cartReducer,
     productReducer,
+    loadingReducer,
   },
   devTools: process.env.NODE_ENV !== "production",
 });
